24a30c33b93f3a21db03ee2019a545b36d58632f
Lenerox <tymek06@wp.pl>
2022-12-05 21:24:29 +0100

    modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Witaj Karolu");
+﻿using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //Console.WriteLine("Witaj Karolu");
+        var x = "tekst";
+        x = Console.ReadLine();
+        Console.WriteLine(x);
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
